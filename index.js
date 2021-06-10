86c7a1c8127bf5d35c86b26851fd61ed92b4e371
Avognon Lionel <avognoncharbel@gmail.com>
2021-06-10 21:11:18 +0000

    achieve

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-let num = 1001;
+let num = 1001.1;
 
 //Returns 'undefined'.
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
 
-
+console.log(num.toString().length);
 
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
-
+console.log(
+  num.toString().indexOf(".")===-1 ?
+   `number of digits = Length = ${num.toString().length}` : `Length ${num.toString().length}  Number of digits = ${num.toString().length +1}`
+  );
 
 
 
